40341ed66cd5ab6c043c2d5a078ab71d4dfbd34f
KIM <152789781+KIMJUNGRYUN@users.noreply.github.com>
2024-12-09 16:09:25 +0900

    Delete SpringBoot 4Days directory

--- SpringBoot 4Days/src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.sbb.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//jsp 할때는 dao crud 메소드를 직접 만들었으나 JAP 는 CRUD 자동완성됨
-//인터페이스 imple 클래스 즉 구현클래스를 자동으로 만들어짐
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-    
-}
